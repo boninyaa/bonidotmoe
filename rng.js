350bb35ef10fe3f728153775070ababf91161413
boni <meow@boni.moe>
2026-09-04 11:58:59 +0300

    update bucket url

--- rng.js
+++ rng.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const rngImg = document.getElementById("rng-img");
   if (rngImg) {
-    const randomNum = Math.floor(Math.random() * 30) + 1;
-    rngImg.src = `https://boobies.shiver.mom/random${randomNum}.png`;
+    const randomNum = Math.floor(Math.random() * 36) + 1;
+    rngImg.src = `https://bunny.boni.moe/rng/${randomNum}.jpg`;
   }
 });
